43cd35ae6da990ca6decc456b5e84bb03a1e3e7a
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2010-06-24 11:07:47 +0200

    Deleted redundant tests

--- CoffeeMachineTest.php
+++ CoffeeMachineTest.php
@@ -35,38 +35,6 @@ class CoffeeMachineTest extends PHPUnit_Framework_TestCase
         $this->assertSuppliesAre(10, 'Chocolate');
     }
 
-    /**
-     * @expectedException PHPUnit_Framework_Error
-     */
-    public function testCoffeeSuppliesMustBeExplicitlyNumbered()
-    {
-        new CoffeeSupply;
-    }
-
-    public static function invalidCoffeeSuppliesNumber()
-    {
-        return array(
-            array(1),
-            array(2),
-            array(3),
-            array(4),
-            array(6), 
-            array(7), 
-            array(8), 
-            array(9), 
-            array(11)
-        );
-    }
-
-    /**
-     * @dataProvider invalidCoffeeSuppliesNumber
-     * @expectedException InvalidArgumentException
-     */
-    public function testCoffeeSuppliesMustComeInMultiplesOfFive($number)
-    {
-        new CoffeeSupply($number);
-    }
-
     private function assertSuppliesAre($number, $beverageName)
     {
         $this->assertEquals($number, $this->machine->getSupplies($beverageName));
